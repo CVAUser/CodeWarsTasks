7887102a13d35cf9813e15717aa5f9aa1d54dd5e
Вячеслав Черкасюк <vyacheslav.ch@itfox-web.com>
2023-04-26 19:44:45 +0300

    create recursive

--- CountingChangeCombinations.py
+++ CountingChangeCombinations.py
@@ -1,15 +1,15 @@
 def count_change(money, coins):
     result = 0
     sum = 0
-    # if(sum == money):
-    #     result += 1
-    # else: 
-    for i in range(len(coins)):
-        if (money % coins[i] == 0):
-            result += 1
-        print(coins[i - 1])
-        # result = result + count_change(money, coins[i:])
-    print(result)
+    if(sum == money):
+        result += 1
+    else: 
+        for i in range(len(coins)):
+            if (money % coins[i] == 0):
+                result += 1
+            # sum = sum + coins[i]
+            result = result + count_change(money, coins[i+1:])
+        # print(result)
     return result
 
-count_change(10, [5,2,3])
+print(count_change(10, [5,2,3]))
